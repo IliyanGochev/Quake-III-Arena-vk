0c36beae3a2fac9df446c607c14769fe8d2b3d2f
Iliyan Gochev <iliyan.gochev@gmail.com>
2019-03-22 16:46:32 +0200

    Enable WIN32 Vulkan specific stuff

--- code/vulkan/vk_common.h
+++ code/vulkan/vk_common.h
@@ -6,7 +6,7 @@ extern "C" {
 #   include "../renderer/tr_layer.h"
 #   include "../qcommon/qcommon.h"
 }
-
+#define VK_USE_PLATFORM_WIN32_KHR  1
 #include <vulkan/vulkan.h>
 
 #define VK_PUBLIC extern "C"
