f1e1d8a9de5759ad83bfeed092f9fa94ec829aad
Iliyan Gochev <iliyan.gochev@gmail.com>
2019-04-05 15:52:33 +0300

    We need the stencil ops for the color attachment

--- code/vulkan/vk_device.cpp
+++ code/vulkan/vk_device.cpp
@@ -405,23 +405,25 @@ void VkCreateRenderPass() {
 	VkAttachmentDescription attachments[2];
 
 	// Depth
-	attachments[0].flags = 0;
-	attachments[0].format =  g_vkDepthStencilFormat;
-	attachments[0].samples = VK_SAMPLE_COUNT_1_BIT;
-	attachments[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-	attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-	attachments[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-	attachments[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_STORE;
-	attachments[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-	attachments[0].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+	attachments[0].flags			= 0;
+	attachments[0].format			= g_vkDepthStencilFormat;
+	attachments[0].samples			= VK_SAMPLE_COUNT_1_BIT;
+	attachments[0].loadOp			= VK_ATTACHMENT_LOAD_OP_CLEAR;
+	attachments[0].storeOp			= VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	attachments[0].stencilLoadOp	= VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	attachments[0].stencilStoreOp	= VK_ATTACHMENT_STORE_OP_STORE;
+	attachments[0].initialLayout	= VK_IMAGE_LAYOUT_UNDEFINED;
+	attachments[0].finalLayout		= VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
 	// Color
-	attachments[1].flags = 0;
-	attachments[1].format =  g_vkSurfaceFormat.format;
-	attachments[1].samples = VK_SAMPLE_COUNT_1_BIT;
-	attachments[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR; // Could be don't care
-	attachments[1].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-	attachments[1].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-	attachments[1].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+	attachments[1].flags			= 0;
+	attachments[1].format			= g_vkSurfaceFormat.format;
+	attachments[1].samples			= VK_SAMPLE_COUNT_1_BIT;
+	attachments[1].loadOp			= VK_ATTACHMENT_LOAD_OP_CLEAR; // Could be don't care
+	attachments[1].storeOp			= VK_ATTACHMENT_STORE_OP_STORE;
+	attachments[1].stencilLoadOp	= VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	attachments[1].stencilStoreOp	= VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	attachments[1].initialLayout	= VK_IMAGE_LAYOUT_UNDEFINED;
+	attachments[1].finalLayout		= VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
 	VkAttachmentReference subpassDepthAttachmentRef{};
 	subpassDepthAttachmentRef.attachment	= 0;
